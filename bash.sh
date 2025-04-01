bf49b185509b832a8cf1e61fda882a3f91d0357f
Yuri Lucatelli Taba <yuri.l.taba@gmail.com>
2025-04-01 18:59:05 -0300

    teste

--- bash.sh
+++ bash.sh
@@ -3,10 +3,10 @@
 set -o errexit
 
 # Install ODBC Driver
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql18
-apt-get install -y unixodbc-dev
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/debian/11/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+sudo apt-get install -y unixodbc-dev
 # Install Python dependencies
 pip install -r requirements.txt
